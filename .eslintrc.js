0e57c35402deca42e5adf45268173bb382a67709
Huy <phamhuy19011999@gmail.com>
2022-10-27 18:28:53 +0700

    Install express and show a welcome message

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,11 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "commonjs": true,
-        "es2021": true
-    },
-    "extends": "eslint:recommended",
-    "rules": {
-		'no-unused-vars': 'off'
-    }
-}
+  env: {
+    node: true,
+    commonjs: true,
+    es2022: true,
+  },
+  extends: 'eslint:recommended',
+  rules: {
+    'no-unused-vars': 'off'
+  },
+};
